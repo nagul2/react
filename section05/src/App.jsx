151bf79a78a2b858db83956a2037ea23ce3d7a24
nagul2 <hayani0000@gmail.com>
2026-01-19 22:26:21 +0900

    컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,8 +1,32 @@
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+// // 화살표 함수로 만든 컴포넌트
+// const Header = () => {
+//   return (
+//     <header>
+//       <h1>header</h1>
+//     </header>
+//   );
+// };
+
+// // 함수 표현식으로 만든 컴포넌트
+// function Header2() {
+//   return (
+//     <header>
+//       <h1>header2</h1>
+//     </header>
+//   );
+// }
 
 function App() {
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
       <h1>안녕 리액트!</h1>
     </>
   );
