3cb61f6ed446911d624dcee6f3e4e35b1d797fc0
nagul2 <hayani0000@gmail.com>
2026-01-22 17:08:20 +0900

    Todo 리스트 만들기 - 렌더링 까지

---
+++ section08/src/components/List.jsx
@@ -0,0 +1,40 @@
+import "./List.css";
+import TodoItem from "./TodoItem.jsx";
+import { useState } from "react";
+
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredDate = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    );
+  };
+
+  const filteredTodos = getFilteredDate();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
---
+++ section08/src/components/TodoItem.jsx
@@ -0,0 +1,15 @@
+import "./TodoItem.css";
+
+const TodoItem = ({ id, isDone, content, date }) => {
+  return (
+    <div className="TodoItem">
+      <input checked={isDone} type="checkbox" />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <div className="date">{}</div>
+      <button>삭제</button>
+    </div>
+  );
+};
+
+export default TodoItem;
