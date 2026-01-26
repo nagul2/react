6dadfde22a7b82597d018cc7f827a0593037b5b5
nagul2 <hayani0000@gmail.com>
2026-01-26 21:28:42 +0900

    최적화

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useReducer, useRef } from "react";
+import { useReducer, useRef, useCallback } from "react";
 
 import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
@@ -45,7 +45,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
       type: "CREATE",
       data: {
@@ -55,21 +55,24 @@ function App() {
         date: new Date().getTime(),
       },
     });
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
+  // 첫 번재 인수: 최적화 하고 싶은 함수, 두 번째 인수: deps
+  // deps가 변경되었을 때 콜백 함수를 그대로 생성해서 반환함 -> 함수를 메모이제이션
+  // deps가 빈 배열이면 컴포넌트가 마운트 될 때에(최초 한 번)만 생성하고 이후에는 리렌더링이 발생해도 함수를 새롭게 생성하지 않음
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId,
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     });
-  };
+  }, []);
 
   return (
     <>
@@ -83,3 +86,31 @@ function App() {
 }
 
 export default App;
+
+// 최적화 전 코드
+
+//   const onCreate = (content) => {
+//     dispatch({
+//       type: "CREATE",
+//       data: {
+//         id: idRef.current++,
+//         isDone: false,
+//         content: content,
+//         date: new Date().getTime(),
+//       },
+//     });
+//   };
+
+//   const onUpdate = (targetId) => {
+//     dispatch({
+//       type: "UPDATE",
+//       targetId: targetId,
+//     });
+//   };
+
+//   const onDelete = (targetId) => {
+//     dispatch({
+//       type: "DELETE",
+//       targetId: targetId,
+//     });
+//   };
--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -20,13 +20,16 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default memo(TodoItem, (prevProps, nextProps) => {
-  // id, isDone, content, date의 값이 변경되었을 때에만 리렌더링 하도록 커스터마이징
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
+export default memo(TodoItem);
 
-  // 전부 안바뀌었으면 리렌더링 X
-  return true;
-});
+// 콜백 함수로 최적화
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // id, isDone, content, date의 값이 변경되었을 때에만 리렌더링 하도록 커스터마이징
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+
+//   // 전부 안바뀌었으면 리렌더링 X
+//   return true;
+// });
