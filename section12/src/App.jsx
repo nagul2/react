73dd2d32993b6d272f90f4f75576785bff7778fd
nagul2 <hayani0000@gmail.com>
2026-01-27 18:32:13 +0900

    감정일기장 - 라우팅 설정하기

---
+++ section12/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import New from "./pages/New.jsx";
+import Diary from "./pages/Diary.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+// 라우팅
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+function App() {
+  return (
+    <>
+      <div>공통 헤더 컴포넌트</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />"
+      </Routes>
+    </>
+  );
+}
+
+export default App;
