61f46d07c3828685a595c0fe4bb322c7f8983943
nagul2 <hayani0000@gmail.com>
2026-01-28 18:15:12 +0900

    일기 관리 기능 구현 1

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,44 +1,42 @@
 import "./App.css";
+import { useReducer } from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import New from "./pages/New.jsx";
 import Diary from "./pages/Diary.jsx";
+import Edit from "./pages/Edit.jsx";
 import NotFound from "./pages/NotFound.jsx";
-import Button from "./components/Button.jsx";
-import Header from "./components/Header.jsx";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, [mockData]);
+
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />"
       </Routes>
     </>
@@ -68,3 +66,30 @@ export default App;
         <img src={"/emotion5.png"} />
       </div>
    */
+
+// 헤더, 버튼 실습
+//         <Header
+//     title={"Header"}
+//     leftChild={<Button text={"left"} />}
+//     rightChild={<Button text={"right"} />}
+//   />
+//   <Button
+//     text={"123"}
+//     onClick={() => {
+//       console.log("123번 버튼 클릭");
+//     }}
+//   />
+//   <Button
+//     text={"123"}
+//     type={"POSITIVE"}
+//     onClick={() => {
+//       console.log("123번 버튼 클릭");
+//     }}
+//   />
+//   <Button
+//     text={"123"}
+//     type={"NEGATIVE"}
+//     onClick={() => {
+//       console.log("123번 버튼 클릭");
+//     }}
+//   />
