b0ef68401ed6a99c50f6956c20f2a57deef74268
nagul2 <hayani0000@gmail.com>
2026-01-28 11:18:09 +0900

    동적 라우팅

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,23 +1,33 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import New from "./pages/New.jsx";
 import Diary from "./pages/Diary.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
-// 라우팅
-
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
     <>
-      <div>공통 헤더 컴포넌트</div>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<NotFound />} />"
       </Routes>
     </>
@@ -25,3 +35,10 @@ function App() {
 }
 
 export default App;
+
+// <a> 태그는 페이지 이동시 새로고침 됨 -> CSR 로 동작하지 않음
+//   <div>
+//     <a href="/">Home</a>
+//     <a href="/new">New</a>
+//     <a href="/diary">Diary</a>
+//   </div>
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -3,3 +3,21 @@ const Home = () => {
 };
 
 export default Home;
+
+// QueryString 실습
+// import { useSearchParams } from "react-router-dom";
+
+// const Home = () => {
+//   // params: QueryString으로 전달한 변수와 값
+//   // setParams: QueryString의 값을 변경할 수 있는 함수
+//   const [params, setParams] = useSearchParams();
+
+//   return (
+//     <div>
+//       Home
+//       <div>{params.get("key")}</div>
+//     </div>
+//   );
+// };
+
+// export default Home;
