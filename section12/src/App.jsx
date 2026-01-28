c7f651352ebf0bf70c00debe3e4c2c665e9bcf51
nagul2 <hayani0000@gmail.com>
2026-01-28 17:33:41 +0900

    공통 컴포넌트 작성

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,29 +1,40 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import New from "./pages/New.jsx";
 import Diary from "./pages/Diary.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
-
   return (
     <>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
-
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"left"} />}
+        rightChild={<Button text={"right"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
@@ -36,9 +47,24 @@ function App() {
 
 export default App;
 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+
 // <a> 태그는 페이지 이동시 새로고침 됨 -> CSR 로 동작하지 않음
 //   <div>
 //     <a href="/">Home</a>
 //     <a href="/new">New</a>
 //     <a href="/diary">Diary</a>
 //   </div>
+
+// public 폴더 아래에 있는 이미지 불러오기
+/* 
+      <div>
+        <img src={"/emotion1.png"} />
+        <img src={"/emotion2.png"} />
+        <img src={"/emotion3.png"} />
+        <img src={"/emotion4.png"} />
+        <img src={"/emotion5.png"} />
+      </div>
+   */
