36aca5183ee875d8f1835bfc623e4531e9e04ae8
nagul2 <hayani0000@gmail.com>
2026-01-29 22:54:52 +0900

    New 페이지 UI 렌더링

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,5 +1,17 @@
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
+
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default New;
