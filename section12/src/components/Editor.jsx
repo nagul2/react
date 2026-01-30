854b94d66c0814a763e2f9f55730dab29fcd6564
nagul2 <hayani0000@gmail.com>
2026-01-30 14:21:12 +0900

    New 페이지 기능 구현

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,5 +1,8 @@
 import "./Editor.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import EmotionItem from "./EmotionItem.jsx";
 import Button from "./Button.jsx";
 
@@ -26,34 +29,101 @@ const emotionList = [
   },
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+// 날짜 -> YYYY-MM-DD
+const getStringedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  // 01 ~ 09 표현
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ onSubmit }) => {
+  const nav = useNavigate();
+
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  // e.target.value는 문자열로 들어옴 -> 날짜인 경우 Date로 변환해야함
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  // 작성 완료 버튼을 동적으로 동작하게 하기 위한 함수
+  // 생성 페이지 -> 새 일기 생성, 수정 페이지 -> 일기 수정
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
           {emotionList.map((item) => (
             <EmotionItem
+              onClick={() => {
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                });
+              }}
               key={item.emotionId}
               {...item}
-              isSelected={item.emotionId === emotionId}
+              isSelected={item.emotionId === input.emotionId}
             />
           ))}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+          placeholder="오늘은 어땠나요?"
+        />
       </section>
       <section className="button_section">
-        <Button text={"취소하기"} />
-        <Button text={"작성완료"} type={"POSITIVE"} />
+        <Button onClick={() => nav(-1)} text={"취소하기"} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={"작성완료"}
+          type={"POSITIVE"}
+        />
       </section>
     </div>
   );
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,15 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App.jsx";
+
 import Header from "../components/Header.jsx";
 import Button from "../components/Button.jsx";
 import Editor from "../components/Editor.jsx";
 
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true }); // 저장 홈으로 이동 이동, replace: true 뒤로가기 방지 -> history추가하지 않고 현재 페이지에 덮어버림
+  };
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
